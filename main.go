5beb4e31e6a39b6ff0b843b0ae08555e69fc20b0
三千 <98223596+sanqian12@users.noreply.github.com>
2025-12-25 16:46:04 +0800

    Update main.go

--- main.go
+++ main.go
@@ -81,7 +81,7 @@ import (
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/bilibili"          // b站相关
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/bilibiliparse"     // b站相关
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/bilibilipush"      // b站相关
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/bookreview"        // 哀伤雪刃吧推书记录
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/bookreview"        // 哀伤雪刃吧推书记录
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/chess"             // 国际象棋
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/choose"            // 选择困难症帮手
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/chouxianghua"      // 说抽象话
@@ -136,28 +136,28 @@ import (
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/realcugan"         // realcugan清晰术
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/reborn"            // 投胎
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/robbery"           // 打劫群友的ATRI币
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/rsshub"            // RSSHub订阅姬
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/runcode"           // 在线运行代码
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/saucenao"          // 以图搜图
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/score"             // 分数
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/setutime"          // 来份涩图
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/shadiao"           // 沙雕app
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/shindan"           // 测定
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/steam"             // steam相关
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/tarot"             // 抽塔罗牌
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/tiangou"           // 舔狗日记
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/tracemoe"          // 搜番
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/translation"       // 翻译
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/wallet"            // 钱包
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/wantquotes"        // 据意查句
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/warframeapi"       // warframeAPI插件
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/wife"              // 抽老婆
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/wordcount"         // 聊天热词
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/wordle"            // 猜单词
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/ygocdb"            // 游戏王白鸽API卡查
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/ygotrade"          // 游戏王集换社卡价查询
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/ymgal"             // 月幕galgame
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/yujn"              // 遇见API
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/rsshub"            // RSSHub订阅姬
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/runcode"           // 在线运行代码
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/saucenao"          // 以图搜图
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/score"             // 分数
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/setutime"          // 来份涩图
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/shadiao"           // 沙雕app
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/shindan"           // 测定
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/steam"             // steam相关
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/tarot"             // 抽塔罗牌
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/tiangou"           // 舔狗日记
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/tracemoe"          // 搜番
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/translation"       // 翻译
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/wallet"            // 钱包
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/wantquotes"        // 据意查句
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/warframeapi"       // warframeAPI插件
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/wife"              // 抽老婆
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/wordcount"         // 聊天热词
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/wordle"            // 猜单词
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/ygocdb"            // 游戏王白鸽API卡查
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/ygotrade"          // 游戏王集换社卡价查询
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/ymgal"             // 月幕galgame
+	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/yujn"              // 遇见API
 
 	// _ "github.com/FloatTech/ZeroBot-Plugin/plugin/wtf"           // 鬼东西
 
